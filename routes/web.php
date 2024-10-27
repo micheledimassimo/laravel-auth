b6a44a2d3a148d9ce320f5941fd01d9e71375c89
micheledimassimo <mcldms@outlook.it>
2024-10-27 16:32:59 +0100

    creata struttura progetto

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 // Controllers
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\Admin\MainController as AdminMainController;
-
+use App\Http\Controllers\Admin\ProjectController as AdminProjectController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -26,6 +26,7 @@ Route::prefix('admin')
 
     Route::get('/dashboard', [AdminMainController::class, 'dashboard'])->name('dashboard');
 
+    Route::resource('', AdminProjectController::class);
 });
 
 require __DIR__.'/auth.php';
