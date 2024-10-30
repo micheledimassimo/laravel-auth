4b2d456846181f48eb7cd3f86e83010f111eed6c
micheledimassimo <mcldms@outlook.it>
2024-10-30 19:59:53 +0100

    completate pagine index e show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,20 +1,18 @@
 @extends('layouts.app')
 
-@section('page-title', 'Index')
+@section('page-title', 'Show')
 
 @section('main-content')
 
     <h1 class="text-center text-dark text-uppercase">
-        Projects
+        Single Project
     </h1>
     <div class="row">
-        @foreach ($projects as $project)
+        
             <div class="col-4">
                 <div class="card">
                     <div class="card-body">
-                        <span>
-                        {{ $project->id }}
-                        </span>
+                        
                         <h4>
                             {{ $project->name }}
                         </h4>
@@ -24,12 +22,9 @@
                         <p>
                             N. collaboratori: {{ $project->workers }}
                         </p>
-                        <a href="{{ route('admin.projects.show', ['project' => $project->id]) }}" class="btn btn-success">
-                            Mostra
-                        </a>
                     </div>
                 </div>
             </div>
-        @endforeach
+        
     </div>
 @endsection
